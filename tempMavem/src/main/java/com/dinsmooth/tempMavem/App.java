96b5aadbe186909dca5d5564ba9086d5afdac11b
dingmaotech <yangshuang812@foxmail.com>
2015-07-18 21:37:46 +0800

    tempmaven

---
+++ tempMavem/src/main/java/com/dinsmooth/tempMavem/App.java
@@ -0,0 +1,13 @@
+package com.dinsmooth.tempMavem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
